ed679e2d83ec11d044f032a16d02c9f0c14c9b40
unknown <tusharsekhri9@gmail.com>
2020-05-23 01:51:42 -0400

    PORT instead of port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 // const express = require('express')
 // const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
