58869fa645d04e7812d2ef072b186fd32cb83255
unknown <tusharsekhri9@gmail.com>
2020-05-23 01:43:08 -0400

    Modifications

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const path = require("path")
 app.use(bodyParser.urlencoded({
     extended: true
   }));
   app.use(bodyParser.json());
-  app.use(express.static("./build"));
-  app.get("/", (req, res) => {
-    res.sendfile(__dirname + "/build/index.html")
+  app.use(express.static(path.join(__dirname, "./build")));
+  app.get("*", (req, res) => {
+    res.sendfile(path.join(__dirname + "/build/index.html"))
 })
  const message_routes = require('./routes/message.js');
 
